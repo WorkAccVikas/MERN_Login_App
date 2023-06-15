27767d45947a27f8ab6ac4c62840623c907c898b
WorkAccVikas <workaccvikas@gmail.com>
2023-06-15 20:25:19 +0530

    FD : Updated React Router DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,51 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// Todo : Import all components
+import Username from "./components/Username";
+import Register from "./components/Register";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
 // Todo : root routes
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Root Route</div>,
+    element: <Username />,
   },
   {
     path: "/register",
-    element: <div>Register Route</div>,
+    element: <Register />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
   },
 ]);
 
 function App() {
   return (
     <main>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </main>
   );
 }
