e87240f6caea6d9066d358bc6f5f540075b47198
WorkAccVikas <workaccvikas@gmail.com>
2023-06-16 15:46:51 +0530

    FD : Reset UI

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -6,12 +6,47 @@ export async function usernameValidate(values) {
   return errors;
 }
 
+// Todo : Validate password on password page
+export async function passwordValidate(values) {
+  const errors = passwordVerify({}, values);
+  return errors;
+}
+
+// Todo : Validate
+export async function resetPasswordValidation(values) {
+  const errors = passwordVerify({}, values);
+
+  if (values.password !== values.confirm_pwd) {
+    errors.exist = toast.error("Password not match...!");
+  }
+
+  return errors;
+}
+
 // Todo : Validate username
 function usernameVerify(error = {}, values) {
   if (!values.username) {
     error.username = toast.error("Username Required...!");
   } else if (values.username.includes(" ")) {
     error.username = toast.error("Invalid Username...!");
+  }
+  return error;
+}
+
+// Todo : Validate username
+function passwordVerify(error = {}, values) {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+  if (!values.password) {
+    error.password = toast.error("Password Required...!");
+  } else if (values.password.includes(" ")) {
+    error.password = toast.error("Wrong Password...!");
+  } else if (values.password.length < 4) {
+    error.password = toast.error(
+      "Password must be more than 4 characters long...!"
+    );
+  } else if (!specialChars.test(values.password)) {
+    error.password = toast.error("Password must have special character...!");
   }
   return error;
 }
