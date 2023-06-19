6298293f978e3f3d3db0e1ced51a9bd479351ef1
WorkAccVikas <workaccvikas@gmail.com>
2023-06-19 15:25:27 +0530

    FD : Password component changes

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -2,17 +2,17 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import avatar from "../assets/profile.png";
 import styles from "../styles/Username.module.css";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { passwordValidate } from "../helper/validate";
 import { useAuthStore } from "../store/store";
 import useFetch from "../hooks/fetch.hook";
+import { verifyPassword } from "../helper/helper";
 
 function Password() {
   const navigate = useNavigate();
   const { username } = useAuthStore((state) => state.auth);
   const [{ isLoading, apiData, serverError }] = useFetch(`/user/${username}`);
-
 
   const formik = useFormik({
     initialValues: {
@@ -23,6 +23,21 @@ function Password() {
     validateOnChange: false,
     onSubmit: async (values) => {
       console.log(values);
+      let loginPromise = verifyPassword({
+        username,
+        password: values.password,
+      });
+      toast.promise(loginPromise, {
+        loading: "Checking...",
+        success: <b>Login Successfully...!</b>,
+        error: <b>Password Not Match!</b>,
+      });
+
+      loginPromise.then((res) => {
+        let { token } = res.data;
+        localStorage.setItem("token", token);
+        navigate("/profile");
+      });
     },
   });
 
