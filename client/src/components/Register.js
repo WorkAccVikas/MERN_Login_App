13ccbb6c03ed56f26596b9a279f788e753d74e02
WorkAccVikas <workaccvikas@gmail.com>
2023-06-19 14:51:23 +0530

    FD : Register component changes

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import avatar from "../assets/profile.png";
 import styles from "../styles/Username.module.css";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { registerValidation } from "../helper/validate";
 import convertToBase64 from "../helper/convert";
-
+import { registerUser } from "../helper/helper";
 function Register() {
+  const navigate = useNavigate();
   const [file, setFile] = useState();
 
   const formik = useFormik({
@@ -22,6 +23,15 @@ function Register() {
     onSubmit: async (values) => {
       values = await Object.assign(values, { profile: file || "" });
       console.log(values);
+      let registerPromise = registerUser(values);
+      toast.promise(registerPromise, {
+        loading: "Creating...",
+        success: <b>Register Successfully...!</b>,
+        error: <b>Could not Register.</b>,
+      });
+      registerPromise.then(function () {
+        navigate("/");
+      });
     },
   });
 
